9ed3f49d7271ab54ca07f2a554c934d012edd8c5
jfesti <joopsdadoops@gmail.com>
2024-05-07 14:01:47 -0700

    Included Unit Tests for User.java and UserDB.java

---
+++ src/main/UserDBTest.java
@@ -0,0 +1,35 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserDBTest {
+
+    private UserDB usDB;
+
+    @BeforeEach
+    public final void setup(){ usDB = new UserDB();
+        usDB.addUser(new User("Bill", "BillsEmail@Email.com"));
+        usDB.addUser(new User("Lola", "LolaEmail@Email.com"));
+    }
+
+    @Test
+    public final void addUserTest(){
+        usDB.addUser(new User("Grandma", "Grandma@gmail.com"));
+        assertEquals("Grandma", usDB.getUserByName("Grandma").getName());
+    }
+
+    @Test
+    public final void getUserByNameTest(){
+        assertEquals("Bill", usDB.getUserByName("Bill").getName());
+    }
+
+    @Test
+    public final void getUserByEmailTest() {
+        assertEquals("Lola", usDB.getUserByEmail("LolaEmail@Email.com").getName());
+    }
+
+
+
+}
---
+++ src/main/UserTest.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User usTest;
+
+    @BeforeEach
+    public final void setup() { usTest = new User ("John", "JSmith@yahoo.com");}
+
+    @Test
+    public final void logInTest() {
+        assertTrue(usTest.logIn("John", "JSmith@yahoo.com"));
+    }
+
+    @Test
+    public final void getNameTest() {
+        assertEquals("John", usTest.getName());
+    }
+
+    @Test
+    public final void getEmailTest() {
+        assertEquals("JSmith@yahoo.com", usTest.getEmail());
+    }
+
+
+}
