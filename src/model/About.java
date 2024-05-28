2fe10dca2e9e4d710c5f16352221e8b2e768e9a2
tianp1n <pinedchr1@gmail.com>
2024-05-28 11:27:28 -0700

    updated version number

--- src/model/About.java
+++ src/model/About.java
@@ -15,7 +15,7 @@ public class About {
     /**
      * Instance field which contains the current version number.
      */
-    private static final double VERSION_NUMBER = 0.1;
+    private static final double VERSION_NUMBER = 0.3;
 
     /**
      * Instance field which contains a list to store developer names.
