4321976a1f3c1104d99bf14b9e31086c141e2b47
Ian <ianpsal@uw.edu>
2024-06-03 07:30:03 -0700

    Added a menu bar to NotesPage.java

--- src/view/NotesPage.java
+++ src/view/NotesPage.java
@@ -15,28 +15,47 @@ public class NotesPage extends JFrame {
     Project myProject;
     JFrame myNotes;
     JTextArea myTextArea;
+    JMenuBar myMenuBar;
+    JMenu myMenuFile;
 
     // The constructor used for testing. Will not be used in the running application.
     public NotesPage() {
+        //Window
         myNotes = new JFrame("Notes");
         myNotes.setSize(700, 500);
         myNotes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //Text area
         myTextArea = new JTextArea();
         myNotes.add(myTextArea);
+
+        //Menu bar
+        myMenuBar = new JMenuBar();
+        myNotes.setJMenuBar(myMenuBar);
+        myMenuFile = new JMenu("File");
+        myMenuBar.add(myMenuFile);
 
         myNotes.setVisible(true);
     }
 
+    // The constructor used by the application.
     public NotesPage(Project theProject) {
         this.myProject = theProject;
 
+        //Window
         myNotes = new JFrame("Notes");
         myNotes.setSize(700, 500);
         myNotes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //Text area
         myTextArea = new JTextArea();
         myNotes.add(myTextArea);
+
+        //Menu bar
+        myMenuBar = new JMenuBar();
+        myNotes.setJMenuBar(myMenuBar);
+        myMenuFile = new JMenu("File");
+        myMenuBar.add(myMenuFile);
 
         myNotes.setVisible(true);
     }
