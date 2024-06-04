8327e9b158b1f103768729b936ecaa26f0856331
Ian <ianpsal@uw.edu>
2024-06-04 05:13:13 -0700

    Added some functionality to the menu bar in NotesPage.java
    Can load the contents of another file.
    Can save files

--- src/view/NotesFile.java
+++ src/view/NotesFile.java
@@ -1,10 +1,15 @@
 package view;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class NotesFile {
 
     NotesPage myNotesPage;
+    String myFileName;
+    String myFileAddress;
 
     public NotesFile(NotesPage theNotesPage) {
         this.myNotesPage = theNotesPage;
@@ -13,11 +18,70 @@ public class NotesFile {
     public void newFile() {
         myNotesPage.myTextArea.setText("");
         myNotesPage.myNotes.setTitle("New");
+        myFileName = null;
+        myFileAddress = null;
     }
 
     public void open() {
         FileDialog fd = new FileDialog(myNotesPage.myNotes, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+
+        if (fd.getFile() != null) {
+            myFileName = fd.getFile();
+            myFileAddress = fd.getDirectory();
+            myNotesPage.myNotes.setTitle(myFileName);
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(myFileAddress + myFileName));
+            myNotesPage.myTextArea.setText("");
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+                myNotesPage.myTextArea.append(line + "\n");
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("File not opened.");
+        }
+    }
+
+    public void save() {
+        if (myFileName == null) {
+            saveAs();
+        } else {
+            try {
+                FileWriter fw = new FileWriter(myFileAddress + myFileName);
+                fw.write(myNotesPage.myTextArea.getText());
+                myNotesPage.myNotes.setTitle(myFileName);
+                fw.close();
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+    }
+
+    public void saveAs() {
+        FileDialog fd = new FileDialog(myNotesPage.myNotes, "Save", FileDialog.SAVE);
+        fd.setVisible(true);
+
+        if(fd.getFile() != null) {
+            myFileName = fd.getFile();
+            myFileAddress = fd.getDirectory();
+            myNotesPage.myNotes.setTitle(myFileName);
+        }
+
+        try {
+            FileWriter fw = new FileWriter(myFileAddress + myFileName);
+            fw.write(myNotesPage.myTextArea.getText());
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Error.");
+        }
+    }
+
+    public void exit() {
+        myNotesPage.dispose();
     }
 
 }
--- src/view/NotesPage.java
+++ src/view/NotesPage.java
@@ -9,11 +9,6 @@ import java.awt.event.ActionListener;
 
 public class NotesPage extends JFrame implements ActionListener {
 
-    // Just for testing
-    public static void main(String[] args) {
-        new NotesPage();
-    }
-
     Project myProject;
     JFrame myNotes;
     JTextArea myTextArea;
@@ -22,52 +17,6 @@ public class NotesPage extends JFrame implements ActionListener {
     JMenu myMenuFile;
     JMenuItem myNew, myOpen, mySave, mySaveAs, myExit;
     NotesFile myNotesFile = new NotesFile(this);
-
-    // The constructor used for testing. Will not be used in the running application.
-    public NotesPage() {
-        //Window
-        myNotes = new JFrame("Notes");
-        myNotes.setSize(700, 500);
-        myNotes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Text area
-        myTextArea = new JTextArea();
-        myNotes.add(myTextArea);
-
-        //Scroll pane
-        myScrollPane = new JScrollPane(myTextArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        myScrollPane.setBorder(BorderFactory.createEmptyBorder());
-        myNotes.add(myScrollPane);
-
-        //Menu bar
-        myMenuBar = new JMenuBar();
-        myNotes.setJMenuBar(myMenuBar);
-        myMenuFile = new JMenu("File");
-        myMenuBar.add(myMenuFile);
-
-        //File menu
-        myNew = new JMenuItem("New");
-        myNew.addActionListener(this);
-        myNew.setActionCommand("New");
-        myMenuFile.add(myNew);
-
-        myOpen = new JMenuItem("Open");
-        myOpen.addActionListener(this);
-        myOpen.setActionCommand("Open");
-        myMenuFile.add(myOpen);
-
-        mySave = new JMenuItem("Save");
-        myMenuFile.add(mySave);
-
-        mySaveAs = new JMenuItem("Save As");
-        myMenuFile.add(mySaveAs);
-        myExit = new JMenuItem("Exit");
-
-        myMenuFile.add(myExit);
-
-        myNotes.setVisible(true);
-    }
 
     // The constructor used by the application.
     public NotesPage(Project theProject) {
@@ -106,15 +55,22 @@ public class NotesPage extends JFrame implements ActionListener {
         myMenuFile.add(myOpen);
 
         mySave = new JMenuItem("Save");
+        mySave.addActionListener(this);
+        mySave.setActionCommand("Save");
         myMenuFile.add(mySave);
 
         mySaveAs = new JMenuItem("Save As");
+        mySaveAs.addActionListener(this);
+        mySaveAs.setActionCommand("SaveAs");
         myMenuFile.add(mySaveAs);
-        myExit = new JMenuItem("Exit");
 
+        myExit = new JMenuItem("Exit");
+        myExit.addActionListener(this);
+        myExit.setActionCommand("Exit");
         myMenuFile.add(myExit);
 
         myNotes.setVisible(true);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     }
 
     @Override
@@ -124,6 +80,9 @@ public class NotesPage extends JFrame implements ActionListener {
         switch(command) {
             case "New": myNotesFile.newFile(); break;
             case "Open": myNotesFile.open(); break;
+            case "Save": myNotesFile.save(); break;
+            case "SaveAs": myNotesFile.saveAs(); break;
+            case "Exit": myNotesFile.exit(); break;
         }
     }
 }
